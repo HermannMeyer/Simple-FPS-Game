9b293dbcabccac4fd544a5afcbe5a2e01c30a2a1
HermannMeyer <49108682+HermannMeyer@users.noreply.github.com>
2021-09-20 21:19:29 -0400

    Add land mines and corresponding activation mechanism

---
+++ Assets/Scripts/Player/Weapons/MineController.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MineController : MonoBehaviour
+{
+    public float blastRadius = 8f;
+    public float damage = 100f;
+    public float explosionForce = 100f;
+
+    //bool isActivated = false;
+    //bool hasExploded = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Enemy")
+        {
+            Explode();
+        }
+    }
+
+    void Explode()
+    {
+        // TODO - Add explosion effect
+
+        // Get all nearby objects
+        Collider[] colliders = Physics.OverlapSphere(transform.position, blastRadius);
+
+        foreach (Collider collider in colliders)
+        {
+            // Apply explosion force to rigidbody if applicable 
+            Rigidbody rigidbody = collider.GetComponent<Rigidbody>();
+            if (rigidbody != null)
+            {
+                rigidbody.AddExplosionForce(explosionForce, transform.position, blastRadius);
+            }
+
+            // Apply damage to targets if applicable
+            Target target = collider.GetComponent<Target>();
+            if (target != null)
+            {
+                target.TakeDamage(damage);
+            }
+
+            // Apply damage to player if applicable
+            PlayerHealth playerHealth = collider.GetComponent<PlayerHealth>();
+            if (playerHealth != null)
+            {
+                playerHealth.TakeDamage(damage);
+            }
+        }
+
+        // Remove the land mine from the scene
+        Destroy(gameObject);
+    }
+}
